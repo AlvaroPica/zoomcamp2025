2879e017c9498e641c726e38aa4324a751d78789
Alvaro Picatoste <apicatoste@gmail.com>
2025-02-10 19:43:26 +0100

    added terraform script

--- 01-docker-terraform/terrademo/variables.tf
+++ 01-docker-terraform/terrademo/variables.tf
@@ -25,14 +25,16 @@ variable "region" {
 variable "bq_dataset_name" {
   description = "My BigQuery Dataset Name"
   #Update the below to what you want your dataset to be called
-  default = "terrademo_dataset"
+  #default = "terrademo_dataset"
+  default = "zoomcamp"
 
 }
 
 variable "gcs_bucket_name" {
   description = "My Storage Bucket Name"
   #Update the below to a unique bucket name
-  default = "angular-rhythm-450212-n4-terrademo-bucket"
+  #default = "angular-rhythm-450212-n4-terrademo-bucket"
+  default = "angular-rhythm-450212-n4-kestra-bucket"
 
 }
 
