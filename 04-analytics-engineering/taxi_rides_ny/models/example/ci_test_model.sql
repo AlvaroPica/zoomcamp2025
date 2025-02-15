e0d9715a62d120c93de3543e24b5ac21b3ba1f7a
Alvaro Picatoste <apicatoste@gmail.com>
2025-02-15 12:47:28 +0100

    First model to test if CI works

---
+++ 04-analytics-engineering/taxi_rides_ny/models/example/ci_test_model.sql
@@ -0,0 +1,13 @@
+WITH source_data AS (
+    SELECT 1 as id, 'A' as letter
+    UNION ALL
+    SELECT 2 as id, 'B' as letter
+    UNION ALL
+    SELECT 3 as id, 'C' as letter
+)
+
+SELECT 
+    id,
+    letter,
+    UPPER(letter) as upper_letter
+FROM source_data 
